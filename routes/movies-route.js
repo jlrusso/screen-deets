1f1e8761b05c2c5499a75030192443f1b954cffe
James Russo <1jlrusso@gmail.com>
2019-01-03 23:50:18 -0800

    remove movies array, add router for nested directories, handle logic with controllers

--- routes/movies-route.js
+++ routes/movies-route.js
@@ -1,26 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+const moviesCtrl = require('../controllers/movies-ctrl');
+const errorCtrl = require('../controllers/error-ctrl');
 
-const movies = [
-  {title: 'Happy Gilmore'},
-  {title: 'Remember the Titans'},
-  {title: 'Never Back Down'},
-  {title: 'Glory'},
-  {title: 'The Matrix'},
-  {title: 'The Matrix Reloaded'},
-  {title: 'The Matrix Revolution'},
-  {title: 'Jackass'},
-  {title: 'Dumb and Dumber'},
-  {title: 'The Dark Knight'},
-  {title: 'Superman'},
-  {title: 'Spiderman'}
-];
-
-router.get("/", (req, res) => {
-  res.render('movies', {movies, pageTitle: 'ScreenDeets | Movies', path: '/movies'});
-  //res.render('movies', {movs: movies, pageTitle: 'ScreenDeets | Movies', path: '/movies'});
-  //res.sendFile(path.join(__dirname, "../views", "movies.html"));
-});
+router.get("/", moviesCtrl.goToMovies);
+router.get('/movie-details/:movieId', moviesCtrl.goToDetails);
+router.use(errorCtrl.goToError);
 
 module.exports = router;
