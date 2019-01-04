058a1a1133a4d72e53929dfe7e7e32f60573b832
James Russo <1jlrusso@gmail.com>
2019-01-03 23:52:35 -0800

    require error and home controllers to handle logic

--- routes/home-route.js
+++ routes/home-route.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+const homeCtrl = require('../controllers/home-ctrl');
+const errorCtrl = require('../controllers/error-ctrl');
 
-router.get("/", (req, res) => {
-  res.render('index', {pageTitle: 'ScreenDeets | Home', path: '/home'});
-  // res.sendFile(path.join(__dirname, '../views', 'index.html'));
-});
-
-router.get("/home", (req, res) => {
-  res.render('index', {pageTitle: 'ScreenDeets | Home', path: '/home'});
-  // res.sendFile(path.join(__dirname, '../views', 'index.html'));
-});
-
-router.use("/", (req, res) => {
-  res.status(404).render('error', {pageTitle: 'ScreenDeets | Error', path: '/'});
-  // res.sendFile(path.join(__dirname, '../views', 'error.html')); 
-});
+router.get('/', homeCtrl.goToHome);
+router.get('/home', homeCtrl.goToHome);
+router.use(errorCtrl.goToError);
 
 module.exports = router;
